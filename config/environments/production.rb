b4a976c799cbf7eb4efbfca0a1d09c961420521a
Chua Jack Yune <jckynchua@yahoo.com>
2016-12-01 16:27:29 +0800

    corrected missing variable address in config/environments production file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,7 +97,7 @@ Rails.application.configure do
     :user_name => ENV["sendgrid_username"],
     :password => ENV["sendgrid_password"],
     :domain => "gmail.com",
-    address => "smtp.sendgrid.net",
+    :address => "smtp.sendgrid.net",
     :port => 587,
     :authentication => "plain",
     :enable_starttls_auto => true
